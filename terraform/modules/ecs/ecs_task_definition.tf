65800b0deb0e80facdcfacb778bf0811fa93c243
sugiyama404 <mmingriha@gmail.com>
2023-03-23 15:44:07 +0900

    up

--- terraform/modules/ecs/ecs_task_definition.tf
+++ terraform/modules/ecs/ecs_task_definition.tf
@@ -7,7 +7,21 @@ resource "aws_ecs_task_definition" "newworld-definition" {
   memory                   = 512
   execution_role_arn       = var.aws_iam_role
   network_mode             = "awsvpc"
-  container_definitions    = file("modules/ecs/container_definitions.json")
+  container_definitions = jsonencode([
+    {
+      name      = "newworld"
+      image     = "280233053143.dkr.ecr.ap-northeast-1.amazonaws.com/newworld:latest"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
